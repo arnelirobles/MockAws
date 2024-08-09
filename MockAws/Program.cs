0acfb8bf40de18e7974f988a36c4d2afcfbc57b3
Arnel Isiderio Robles <arnelirobles@outlook.com>
2024-08-09 10:25:37 +0800

    Add project files.

---
+++ MockAws/Program.cs
@@ -0,0 +1,38 @@
+﻿using Amazon.S3;
+using Amazon.S3.Model;
+using Moq;
+
+namespace MockAws
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var mockS3Client = new Mock<IAmazonS3>();
+
+            var s3Objects = new List<S3Object>
+            {
+                new S3Object { Key = "image1.jpg" },
+                new S3Object { Key = "image2.jpg" }
+            };
+
+            var listObjectsResponse = new ListObjectsResponse { S3Objects = s3Objects };
+
+            mockS3Client
+                .Setup(client => client.ListObjectsAsync(It.IsAny<ListObjectsRequest>(), default))
+                .ReturnsAsync(listObjectsResponse);
+
+            mockS3Client
+                .Setup(client => client.GetPreSignedURL(It.IsAny<GetPreSignedUrlRequest>()))
+                .Returns("http://fake-s3-url.com/image.jpg");
+
+            var service = new TestService(mockS3Client.Object);
+            var images = service.ListImages("prefix").GetAwaiter().GetResult();
+
+            foreach (var image in images)
+            {
+                Console.WriteLine($"ImageURL: {image}");
+            }
+        }
+    }
+}
---
+++ MockAws/TestService.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Amazon.S3;
+using Amazon.S3.Model;
+
+namespace MockAws
+{
+    public class TestService
+    {
+        private readonly IAmazonS3 _s3Client;
+
+        public TestService(IAmazonS3 s3Client)
+        {
+            _s3Client = s3Client;
+        }
+
+        public async Task<List<string>> ListImages(string prefix)
+        {
+            var imageUrls = new List<string>();
+
+            var request = new ListObjectsRequest
+            {
+                BucketName = "your-bucket-name",
+                Prefix = prefix
+            };
+
+            var response = await _s3Client.ListObjectsAsync(request);
+
+            foreach (var obj in response.S3Objects)
+            {
+                var urlRequest = new GetPreSignedUrlRequest
+                {
+                    BucketName = "your-bucket-name",
+                    Key = obj.Key,
+                    Expires = DateTime.UtcNow.AddHours(1)
+                };
+
+                var url = _s3Client.GetPreSignedURL(urlRequest);
+                imageUrls.Add(url);
+
+                if (imageUrls.Count == 2)
+                {
+                    break;
+                }
+            }
+
+            return imageUrls;
+        }
+    }
+}
